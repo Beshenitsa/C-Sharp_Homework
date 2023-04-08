f8b1037c3acf658cd2ca0edfbbdb09bc969edc25
unknown <qvenskaya@gmail.com>
2023-04-08 23:22:06 +0700

    Task 1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Homework 1 Seminar 1
+
+//Task 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+
+Console.Write("Input first number, please: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number, please ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.Write(num1 + " is bigger number. And " + num2 + " is smaller number.");
+else
+    Console.Write(num2 + " is bigger number. And " + num1 + " is smaller number.");
+
+
+//Task 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//Task 3. Напишите программу, которая на вход принимает число и выдаёт, яаляется ли число чётным (делится ли оно на два без остатка).
+
+//Task 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
