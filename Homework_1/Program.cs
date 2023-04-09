a0d1b0cf4b26199ba83d30ac4808c6b3dc03a9d8
unknown <qvenskaya@gmail.com>
2023-04-09 23:07:49 +0700

    Task 2

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -2,18 +2,38 @@
 
 //Task 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
 
-Console.Write("Input first number, please: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number, please ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input first number, please: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number, please ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-    Console.Write(num1 + " is bigger number. And " + num2 + " is smaller number.");
-else
-    Console.Write(num2 + " is bigger number. And " + num1 + " is smaller number.");
+// if (num1 > num2)
+//     Console.Write(num1 + " is bigger number. And " + num2 + " is smaller number.");
+// else
+//     Console.Write(num2 + " is bigger number. And " + num1 + " is smaller number.");
 
 
 //Task 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Input first number, please: ");
+// int a1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number, please ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third number, please ");
+// int c1 = Convert.ToInt32(Console.ReadLine());
+
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+
+// int max = Max(a1, b1, c1);
+
+// Console.WriteLine(max);
+
 
 //Task 3. Напишите программу, которая на вход принимает число и выдаёт, яаляется ли число чётным (делится ли оно на два без остатка).
 
